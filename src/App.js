2dc39fe263bda62028539fccff135529c91112fa
mathlord2 <erixrekt69@gmail.com>
2023-11-27 22:32:43 -0800

    Todo functionality done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import ToDo from "./ToDo";
 import './App.css';
 
 function App() {
+  // Stores all the to-do items
+  const [toDo, setToDo] = useState(["Clean my room"]);
+
+  // Stores the user input
+  const [input, setInput] = useState("");
+
+  // Method for updating the user input value
+  const updateInput = (e) => {
+    setInput(e.target.value);
+  }
+
+  // Method for adding a to-do list item
+  const addToDo = () => {
+    if (input != "") {
+      const updatedToDo = [...toDo, input];
+      setToDo(updatedToDo);
+      setInput("");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input">
+        <input placeholder="What's on your mind?" value={input} onChange={updateInput}/>
+        <button onClick={addToDo}>
+          + Add
+        </button>
+      </div>
+
+      <div className="toDos">
+        {toDo.map(t => <ToDo text={t} key={Math.random()}/>)}
+      </div>
     </div>
   );
 }
